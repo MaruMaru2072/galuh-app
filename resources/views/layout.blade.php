8cc621fb15f7c02bb0bdc31765d4126ca00c1399
MaruMaru2072 <118572688+MaruMaru2072@users.noreply.github.com>
2024-06-28 17:02:26 +0700

    design

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -29,8 +29,8 @@
         <div class="container">
             <nav class="navbar navbar-expand-lg">
                 <a class="navbar-brand" href="/">
-                    <img src="path/to/logo.png" alt="Pet Shop Logo" style="height: 40px; margin-right: 10px;">
-                    Pet Shop
+                    <img src="images/text.png" alt="Pet Shop Logo" style="height: 40px; margin-right: 10px;">
+                    Galuh Petshop
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
